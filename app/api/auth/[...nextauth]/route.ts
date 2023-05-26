d19195d7945bf54149337ceb5cb1e5d305d4fdcc
gretoq <dovhun3d@gmail.com>
2023-05-26 19:48:38 +0300

    Create user model, implement auth and connect MongoDB

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/user';
+
+import { connectToDB } from '@utils/database';
+
+global.console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: `${process.env.GOOGLE_ID}`,
+      clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session?.user?.email });
+
+      global.console.log('session user:', sessionUser);
+      global.console.log('session user before:', session.user);
+
+      // @ts-expect-error
+      session?.user.id = sessionUser._id.toString();
+
+      global.console.log('session user after:', session.user);
+      global.console.log('session:', session);
+
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        const userExists = await User.findOne({ email: profile?.email });
+
+        global.console.log('profile:', profile);
+        global.console.log('user exists:', userExists);
+
+        if (!userExists) {
+          const createdUser = await User.create({
+            email: profile?.email,
+            username: profile?.name?.replace(' ', '').toLowerCase(),
+            image: profile?.image,
+          });
+
+          global.console.log('created User:', createdUser);
+        }
+
+        return true;
+      } catch (error: any) {
+        global.console.log('Error checking if user exists: ', error.message);
+
+        return false;
+      }
+    },
+  },
+});
+
+export {handler as GET, handler as POST };
