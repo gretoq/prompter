63ca40292c99f8c238bbcdf949985d8ae5290899
Dovhun Artem <dovhun3d@gmail.com>
2023-06-06 19:04:27 +0300

    add revalidate

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -13,7 +13,7 @@ const Feed: React.FC = () => {
     error,
     isValidating,
   } = useSWR('/api/prompt', async(url) => {
-    const response = await fetch(url);
+    const response = await fetch(url, {next: { revalidate: 10 }});
     const data: Post[] = await response.json();
     return data;
   });
