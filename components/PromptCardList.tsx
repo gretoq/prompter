77f6953a626448254e99edfa94c454748a0c14c9
Dovhun Artem <dovhun3d@gmail.com>
2023-06-11 15:30:21 +0300

    Create csstransition

--- components/PromptCardList.tsx
+++ components/PromptCardList.tsx
@@ -1,6 +1,11 @@
-import { Post } from '../types/Post';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+
 import React from 'react';
 import PromptCard from './PromptCard';
+import { Post } from '../types/Post';
 
 interface Props {
   posts: Post[],
@@ -16,18 +21,36 @@ const PromptCardList: React.FC<Props> = ({
   onDelete,
 }) => {
   return (
-    <div className="mt-16 prompt_layout">
-      {posts.map(post => {
-        return (
-          <PromptCard
-            key={post._id}
-            post={post}
-            handleTagClick={handleTagClick}
-            onEdit={onEdit}
-            onDelete={onDelete}
-          />
-        );
-      })}
+    <div >
+      <TransitionGroup className="mt-16 prompt_layout">
+        {posts.map(post => {
+          return (
+            <CSSTransition
+              key={post._id}
+              timeout={300}
+              // classNames="item"
+              classNames={{
+                // appear: 'your tailwindcss codes',
+                // appearActive: 'your tailwindcss codes',
+                // appearDone: 'your tailwindcss codes',
+                enter: 'opacity-0',
+                enterActive: 'opacity-100 transition-opacity easy-in duration-500',
+                // enterDone: 'your tailwindcss codes',
+                exit: 'opacity-100',
+                exitActive: 'opacity-0 transition-opacity easy-in duration-500',
+                // exitDone: 'your tailwindcss codes',
+              }}
+            >
+              <PromptCard
+                post={post}
+                handleTagClick={handleTagClick}
+                onEdit={onEdit}
+                onDelete={onDelete}
+              />
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
     </div>
   );
 };
